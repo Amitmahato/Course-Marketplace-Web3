5f69c63b3d7930340e17b0a17fc9a1c69accdad2
Amit Mahato <amit643.mahato@gmail.com>
2022-09-05 15:14:17 +0545

    20.6 More Components - common: hero, breadcrumbs - web3: wallet bar, ethereum rates

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -1,2 +1,4 @@
 export { default as Footer } from "./footer";
 export { default as NavBar } from "./navbar";
+export { default as Breadcrumb } from "./breadcrum";
+export { default as Hero } from "./hero";
---
+++ components/web3/index.tsx
@@ -0,0 +1,2 @@
+export { default as EthereumRate } from "./ethereumRate";
+export { default as Wallet } from "./walletbar";
