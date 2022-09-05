b4da3f8d08a82e9bc0eeaa1bfc9ee5195e851ab4
Amit Mahato <amit643.mahato@gmail.com>
2022-09-05 15:39:22 +0545

    20.8 Course Components - Course Hero, Keypoints, Curriculam & Modal

--- components/course/index.tsx
+++ components/course/index.tsx
@@ -1,1 +1,4 @@
+export { default as CourseCurriculam } from "./curriculam";
 export { default as CourseList } from "./list";
+export { default as CourseHero } from "./hero";
+export { default as CourseKeypoints } from "./keypoints";
