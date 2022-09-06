b07b67a490c6df2144aa5facb85d0e34df7c7ef6
Amit Mahato <amit643.mahato@gmail.com>
2022-09-06 13:03:37 +0545

    21.5 Get Course Details - make course detial page use dynamic link - use next/link to navigate to course detail page - modify navbar with links to take to homepage with other options

--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 export default function NavBar() {
   return (
@@ -6,38 +7,28 @@ export default function NavBar() {
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between">
             <div>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Product
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Features
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Marketplace
-              </a>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Home
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Marketplace
+                </a>
+              </Link>
             </div>
             <div>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Company
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-indigo-600 hover:text-indigo-500"
-              >
-                Log in
-              </a>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Blogs
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Wishlist
+                </a>
+              </Link>
             </div>
           </div>
         </nav>
