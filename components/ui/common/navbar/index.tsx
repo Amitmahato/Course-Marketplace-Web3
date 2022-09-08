5c55da95a6454d8492455d9f236a4f3e6e51d23c
Amit Mahato <amit643.mahato@gmail.com>
2022-09-09 00:34:01 +0545

    23.4 Display address below nav bar under connect/greeting button

--- components/ui/common/navbar/index.tsx
+++ components/ui/common/navbar/index.tsx
@@ -13,7 +13,6 @@ export default function NavBar() {
 
   return (
     <section>
-      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between">
@@ -58,6 +57,13 @@ export default function NavBar() {
           </div>
         </nav>
       </div>
+      {account && (
+        <div className="flex justify-end pt-3 sm:px-6 lg:px-8">
+          <div className="text-white bg-indigo-600 rounded-md p-2">
+            {account}
+          </div>
+        </div>
+      )}
     </section>
   );
 }
