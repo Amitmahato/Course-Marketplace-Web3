d171c068562e0f8d964f56e406beeb2594bc62c8
Amit Mahato <amit643.mahato@gmail.com>
2022-09-09 01:10:38 +0545

    23.7 Hash admin address using keccak256 ecoding and compare with current account's encoded address

--- components/providers/web3/hooks/useAccount.ts
+++ components/providers/web3/hooks/useAccount.ts
@@ -3,8 +3,9 @@ import { useEffect } from "react";
 import useSWR from "swr";
 import Web3 from "web3";
 
+// keccack256 encoding for 0x95bDbD553b5848E10DDE013E32FfdDd9C28BD406 address
 const adminAddress = {
-  "0x95bDbD553b5848E10DDE013E32FfdDd9C28BD406": true,
+  "0x8ae6147204f29e745f1add9c189e17ea9f82c94a861bccd331fd3ab82d152571": true,
 };
 
 export const handler = (web3: Web3, provider: any) => (): IUseAccount => {
@@ -31,7 +32,7 @@ export const handler = (web3: Web3, provider: any) => (): IUseAccount => {
   return {
     account: {
       data,
-      isAdmin: data ? adminAddress[data] : false,
+      isAdmin: data ? adminAddress[web3.utils.keccak256(data)] : false,
       mutate,
       ...swrResponse,
     },
