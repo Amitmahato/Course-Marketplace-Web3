230406eeb4751d7576bbe5d37b7c08892d520716
Amit Mahato <amit643.mahato@gmail.com>
2022-09-09 14:11:35 +0545

    24.3 Chain Id - use web3.eth.getChainId to get the connected chain id - map chain id to actual network name - actual network name is displayed in wallet bar

--- components/providers/web3/hooks/useNetwork.ts
+++ components/providers/web3/hooks/useNetwork.ts
@@ -3,20 +3,30 @@ import { useEffect } from "react";
 import useSWR from "swr";
 import Web3 from "web3";
 
+const NETWORKS = {
+  1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  1337: "Ganache",
+};
+
 export const handler =
   (web3: Web3, provider: any): (() => IUseNetwork) =>
   () => {
     const { mutate, ...swrResponse } = useSWR(
       web3 ? "web3-network" : null,
       async () => {
-        const netId = await web3.eth.net.getId();
-        return netId;
+        const chainId = await web3.eth.getChainId();
+        return NETWORKS[chainId];
       }
     );
 
     const networkListener = (provider: any) => {
-      provider.on("chainChanged", (networkId: number) => {
-        mutate(networkId ?? 0);
+      provider.on("chainChanged", (chainId: string) => {
+        mutate(NETWORKS[parseInt(chainId, 16)] ?? "");
       });
     };
 
