86ffa6357008a7f6d18ed8a8ef2ae1ddaf6db818
Amit Mahato <amit643.mahato@gmail.com>
2022-09-12 23:34:25 +0545

    28.2 Purchase Course function definition partially - use remix to test courseHash generation - reset truffle migration

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -20,4 +20,42 @@ contract CourseMarketplace {
     address owner; // 20 bytes (slot 4)
     State state; // 1 byte (slot 4)
   }
+
+  /**
+    Example,
+      - courseId  = 10 (ASCII) 
+                  = 0x3130 (HEX) 
+      => 16 bytes = 0x00000000000000000000000000003130 (32 characters)
+
+      - address = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 => 20 bytes (30 characters)
+
+      - abi.encodePacked(courseId, msg.sender)
+                  = 00000000000000000000000000003130Ab8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+      
+      - keccak256(00000000000000000000000000003130Ab8483F64d9C6d1EcF9b849Ae677dD3315835cb2)
+                  = ff97578d8dd58d484dac933924a0f06f3ff39cc2843610754dfebd8770f807e5
+                  = courseHash
+                  = ff97578d8dd58d484dac933924a0f06f3ff39cc2843610754dfebd8770f807e5 (Output received from purchaseCourse function when run with above parameters on remix editor)
+      
+      - let proof = courseId-courseId (16 + 16 = 32 bytes, random value as of now)
+                  = 0x0000000000000000000000000000313000000000000000000000000000003130
+   */
+  function purchaseCourse(bytes16 courseId, bytes32 proof)
+    external
+    payable
+    returns (bytes32)
+  {
+    /**
+        - abi.encodePacked(courseId, msg.sender) => simply concatenates courseId & sender address together
+        - keccak256 encodes the concatenated courseId & sender address to generate the final course hash
+     */
+    bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+    return courseHash;
+  }
 }
+
+/**
+  Commands:
+    - const instance = await CourseMarketplace.deployed();
+    - instance.purchaseCourse("0x00000000000000000000000000003130","0x0000000000000000000000000000313000000000000000000000000000003130")
+ */
