0821795d5905e59489f2f5856db7a786151c4076
Amit Mahato <amit643.mahato@gmail.com>
2022-09-15 14:53:38 +0545

    31.6 Owned Course Card Improvement

--- components/ui/course/card/OwnedCourseCard.tsx
+++ components/ui/course/card/OwnedCourseCard.tsx
@@ -20,15 +20,15 @@ const OwnedCourseCard: React.FC<React.PropsWithChildren & IOwnedCourseCard> = ({
   return (
     course && (
       <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-        <div className="flex">
+        <div className="block sm:flex">
           <div className="flex-1">
-            <div className="h-full next-image-wrapper">
+            <div className="h-72 sm:h-full next-image-wrapper">
               <Image
                 className="object-cover"
                 src={course.coverImage}
                 layout="responsive"
                 width="200"
-                height="240"
+                height="275"
               />
             </div>
           </div>
@@ -49,7 +49,7 @@ const OwnedCourseCard: React.FC<React.PropsWithChildren & IOwnedCourseCard> = ({
 
             <div className="border-t border-gray-200">
               <dl>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:px-6">
                   <dt className="text-sm font-medium text-gray-500">
                     Course ID
                   </dt>
@@ -57,9 +57,9 @@ const OwnedCourseCard: React.FC<React.PropsWithChildren & IOwnedCourseCard> = ({
                     {course.ownedCourseId}
                   </dd>
                 </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
+                <div className="bg-white px-4 py-5 sm:grid sm:px-6">
                   <dt className="text-sm font-medium text-gray-500">Proof</dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  <dd className="mt-1 text-sm break-words text-gray-900 sm:mt-0 sm:col-span-2">
                     {course.proof}
                   </dd>
                 </div>
