051aa96087d1396459f704bc189d6b1952351c50
Amit Mahato <amit643.mahato@gmail.com>
2022-09-16 14:56:34 +0545

    33.4 Managed Card Values - display the values of all the fields in the card - set background color to gray & white alternatively - add utility function to convert camel case to title case string

--- components/ui/course/card/ManagedCourseCard.tsx
+++ components/ui/course/card/ManagedCourseCard.tsx
@@ -1,9 +1,14 @@
+import { camelCaseToTitleCaseString } from "@utils/utilityFunctions";
 import { IManagedCourse } from "interfaces/course";
 import React from "react";
 
-const Item: React.FC<{ title: string; value: string }> = ({ title, value }) => {
+const Item: React.FC<{ title: string; value: string; className?: string }> = ({
+  title,
+  value,
+  className = "bg-gray-50",
+}) => {
   return (
-    <div className="bg-gray-50 px-4 py-5 sm:grid sm:px-6">
+    <div className={`px-4 py-5 sm:grid sm:px-6 ${className}`}>
       <div className="text-sm font-medium text-gray-500">{title}</div>
       <div className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
         {value}
@@ -23,8 +28,15 @@ const ManagedCourseCard: React.FC<
     course && (
       <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
         <div className="border-t border-gray-200">
-          <Item title="Course ID" value={course.ownedCourseId} />
-          <Item title="Proof" value={course.proof} />
+          {Object.keys(course)
+            .filter((key) => course[key])
+            .map((key, index) => (
+              <Item
+                title={camelCaseToTitleCaseString(key)}
+                value={course[key]}
+                {...(index % 2 === 1 ? { className: "bg-white-50" } : {})}
+              />
+            ))}
           <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
         </div>
       </div>
