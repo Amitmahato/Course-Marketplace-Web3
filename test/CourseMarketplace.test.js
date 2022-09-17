7ff60cc855166c72a78048ffbacad3f7a92d1009
Amit Mahato <amit643.mahato@gmail.com>
2022-09-18 01:46:36 +0545

    34.4 Add test to test purchaed course data

--- test/CourseMarketplace.test.js
+++ test/CourseMarketplace.test.js
@@ -19,6 +19,8 @@ contract("CourseMarketplace", (accounts) => {
   });
 
   describe("Purchase a new course", () => {
+    let courseHash;
+
     before(async () => {
       await _contract.purchaseCourse(courseId, proof, {
         from: buyer,
@@ -28,7 +30,7 @@ contract("CourseMarketplace", (accounts) => {
 
     it("should get the purchased course at index", async () => {
       const index = 0;
-      const courseHash = await _contract.getCourseHashAtIndex(index);
+      courseHash = await _contract.getCourseHashAtIndex(index);
 
       const expectedHash = web3.utils.soliditySha3(
         {
@@ -45,6 +47,39 @@ contract("CourseMarketplace", (accounts) => {
         courseHash,
         expectedHash,
         "Course hash doesn't match with the hash of the purchased course!"
+      );
+    });
+
+    it("should match the purchase data of the course purchased by the buyer", async () => {
+      const expectedIndex = 0;
+      const expectedState = 0;
+
+      const course = await _contract?.getCourseByHash(courseHash);
+
+      assert.equal(
+        course.id,
+        expectedIndex,
+        `Purchased course index should be ${expectedIndex}`
+      );
+      assert.equal(
+        course.price,
+        value,
+        `Purchased course price should be ${value}`
+      );
+      assert.equal(
+        course.proof,
+        proof,
+        `Purchased proof price should be ${proof}`
+      );
+      assert.equal(
+        course.owner,
+        buyer,
+        `Purchased course buyer should be ${buyer}`
+      );
+      assert.equal(
+        course.state,
+        expectedState,
+        `Purchase course state should be ${expectedState}`
       );
     });
   });
