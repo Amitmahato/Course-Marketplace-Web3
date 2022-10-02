3e5384054d2390382dbfe63a7cee1031acbed8c8
Amit Mahato <amit643.mahato@gmail.com>
2022-10-02 09:04:12 +0545

    40.7 Transfer ownership to main account - send all the remaining rETH from deployer account to the main account - add keccak256 encoded value to admin addresses in useAccount hook - use ropsten chain & network id in development environment

--- components/providers/web3/hooks/useAccount.ts
+++ components/providers/web3/hooks/useAccount.ts
@@ -3,9 +3,10 @@ import { useEffect } from "react";
 import useSWR from "swr";
 import Web3 from "web3";
 
-// keccack256 encoding for 0x95bDbD553b5848E10DDE013E32FfdDd9C28BD406 address
+// keccack256 encoding - 2nd one for 0x95bDbD553b5848E10DDE013E32FfdDd9C28BD406 address
 const adminAddress = {
   "0xdfc570d0aee530c31df1e5bbe7b16751900cfc45de8add8c5b171632ae39014d": true,
+  "0x8ae6147204f29e745f1add9c189e17ea9f82c94a861bccd331fd3ab82d152571": true,
 };
 
 export const handler = (web3: Web3, provider: any) => (): IUseAccount => {
@@ -28,10 +29,12 @@ export const handler = (web3: Web3, provider: any) => (): IUseAccount => {
     }
   }, [provider]);
 
+  const isAdmin = data ? adminAddress[web3.utils.keccak256(data)] : false;
+  debugger;
   return {
     account: {
       data,
-      isAdmin: data ? adminAddress[web3.utils.keccak256(data)] : false,
+      isAdmin,
       mutate,
       ...swrResponse,
     },
